17a303d2e6c2eb21d7e3c5b63f921734db9eecb7
Steve Silvius <ssilvius35@gmail.com>
2015-06-21 09:16:25 -0400

    completed proramming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These  two functions can be used to find and cache the inverse of a
+## matrix, potentially saving time when repeating costly calculations
 
-## Write a short comment describing this function
+## the first function creates four functions that can be used to cache and
+## retrieve the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y){
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) inv <<- solve
+	getinverse <- function() inv
+	list(set = set, get = get,
+		setinverse = setinverse, 
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## the second function will return the inverse matrix, saving time, when 
+## possible by returning a cached result instead of re-computing.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+        	message("getting cached data")
+        	return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
